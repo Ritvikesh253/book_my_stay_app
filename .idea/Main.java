1cca508e550f8be1b5270dfb0cda253faff3c385
rashi0506062906-star <rashi050606.2906@gmail.com>
2026-03-16 09:20:19 +0530

    UC3: Centralized Room Inventory Management

---
+++ .idea/Main.java
@@ -0,0 +1,47 @@
+﻿import java.util.HashMap;
+import java.util.Map;
+
+class RoomInventory {
+    private Map<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+    }
+
+    public void addRoomType(String type, int count) {
+        inventory.put(type, count);
+    }
+
+    public int getAvailability(String type) {
+        return inventory.getOrDefault(type, 0);
+    }
+
+    public void updateAvailability(String type, int count) {
+        if (inventory.containsKey(type)) {
+            inventory.put(type, count);
+        }
+    }
+
+    public void displayInventory() {
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
+
+public class UseCase3InventorySetup {
+    public static void main(String[] args) {
+        RoomInventory inventory = new RoomInventory();
+
+        inventory.addRoomType("Standard", 10);
+        inventory.addRoomType("Deluxe", 5);
+        inventory.addRoomType("Suite", 2);
+
+        inventory.displayInventory();
+
+        inventory.updateAvailability("Standard", 8);
+
+        System.out.println("Updated Inventory:");
+        inventory.displayInventory();
+    }
+}
